76f4ed1bec830dc300b757f8abd7be8a27d8570d
Alessandro Ruiz Ruiz <less013015@gmail.com>
2023-05-22 12:04:50 +0200

    Landing Page

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::get('/landing', function () {
     return Inertia::render('Landing');
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,6 @@ module.exports = {
     }
   },
 
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [require('@tailwindcss/forms')],
+  darkMode: ''
 }
